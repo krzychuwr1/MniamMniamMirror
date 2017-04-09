6921c9726f8de6bb394941da2c1bdcf2a26c7aed
Krzysztof Suruło <krzysztof.surulo@gmail.com>
2017-04-09 19:58:39 +0200

    Switching to add reciepie activity after clicking add button

--- app_android/app/src/main/java/io/iiet/agh/edu/pl/mniammniam/AddRecipeActivity.java
+++ app_android/app/src/main/java/io/iiet/agh/edu/pl/mniammniam/AddRecipeActivity.java
@@ -2,6 +2,7 @@ package io.iiet.agh.edu.pl.mniammniam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.TextView;
 
 public class AddRecipeActivity extends AppCompatActivity {
@@ -12,5 +13,11 @@ public class AddRecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
         mTitle= (TextView) findViewById(R.id.tv_recipe_title);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add, menu);
+        return true;
     }
 }
--- app_android/app/src/main/java/io/iiet/agh/edu/pl/mniammniam/MainActivity.java
+++ app_android/app/src/main/java/io/iiet/agh/edu/pl/mniammniam/MainActivity.java
@@ -1,5 +1,6 @@
 package io.iiet.agh.edu.pl.mniammniam;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,12 +19,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton add_reciepe = (FloatingActionButton) findViewById(R.id.add_reciepe);
+        add_reciepe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent=new Intent(MainActivity.this, AddRecipeActivity.class);
+                startActivity(intent);
             }
         });
     }
