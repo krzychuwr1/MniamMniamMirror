3835ce61df9d780298f8910af0dc187906fccc3b
Krzysztof Wrona <krzychuwr1@gmail.com>
2017-04-17 01:50:00 +0200

    Added review model

---
+++ Server/MniamMniam/Models/CookBookModels/Review.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MniamMniam.Models.CookBookModels
+{
+    public class Review
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Range(1, 5)]
+        public int Score { get; set; }
+
+        public string Text { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int RecipeId { get; set; }
+
+        [ScaffoldColumn(false)]
+        public Recipe Recipe { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int ApplicationUserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
