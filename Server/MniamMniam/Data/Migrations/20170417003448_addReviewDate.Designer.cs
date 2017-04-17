bc4f316de0634ed621385541d8dc81b49a5e540a
Krzysztof Wrona <krzychuwr1@gmail.com>
2017-04-17 02:35:18 +0200

    Add review date db migration

--- Server/MniamMniam/Data/Migrations/20170417003448_addReviewDate.Designer.cs
+++ Server/MniamMniam/Data/Migrations/20170417003448_addReviewDate.Designer.cs
@@ -8,9 +8,10 @@ using MniamMniam.Data;
 namespace MniamMniam.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170417003448_addReviewDate")]
+    partial class addReviewDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
