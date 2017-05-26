a960aaa4384343a6bc8695773d2169bb5899c257
Krzysztof Wrona <krzychuwr1@gmail.com>
2017-05-26 19:43:36 +0200

    Added some controller tests

--- Server/MniamMniamTests/Controllers/RecipesControllerTests.cs
+++ Server/MniamMniamTests/Controllers/RecipesControllerTests.cs
@@ -6,6 +6,8 @@ using Moq;
 using System.Linq;
 using MniamMniam.Models.CookBookModels;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace MniamMniamTests.Controllers
 {
@@ -13,19 +15,36 @@ namespace MniamMniamTests.Controllers
     public class RecipesControllerTests
     {
         Mock<IRecipesRepository> recipesRepo = new Mock<IRecipesRepository>();
-
+        
         [TestMethod]
         public void IndexTest()
         {
-            recipesRepo.Setup(repo => repo.GetAllRecipes()).Returns(
-                new List<Recipe>()
-                    {
-                        new Recipe() { }
-                    });
+            recipesRepo.Setup(repo => repo.GetAllRecipes()).Returns(recipes);
 
             var controller = new RecipesController(null, null, recipesRepo.Object, null, null, null, null);
-            
-            var result = controller.Index();
+
+            var result = controller.Index() as ViewResult;
+
+            Assert.AreEqual(result.Model, recipes);
+        }
+
+        [TestMethod]
+        public async Task AdvancedSearchTest()
+        {
+            string recText = "Recipe1Text";
+
+            var filteredRecipes = recipes.Where(r => r.Text == recText);
+
+            recipesRepo.Setup(repo => 
+                repo
+                .GetFilteredRecipes("", recText, "", "", "", "", ""))
+                .Returns(filteredRecipes);
+
+            var controller = new RecipesController(null, null, recipesRepo.Object, null, null, null, null);
+
+            var result = await controller.advancedSearch(null, recText, null, null, null) as ViewResult;
+
+            Assert.AreEqual(result.Model, filteredRecipes);
         }
 
         [TestMethod]
@@ -40,7 +59,25 @@ namespace MniamMniamTests.Controllers
 
             var controller = new RecipesController(null, null, null, null, null, null, null);
             
-            var result = controller.Index();
+            controller.Index();
         }
+
+        private List<Recipe> recipes = new List<Recipe>()
+            {
+                new Recipe()
+                {
+                    Id = 1,
+                    Name = "Recipe1",
+                    Text = "Recipe1Text",
+                    ApplicationUserId = Guid.NewGuid().ToString()
+                },
+                new Recipe()
+                {
+                    Id = 2,
+                    Name = "Recipe2",
+                    Text = "Recipe2Text",
+                    ApplicationUserId = Guid.NewGuid().ToString()
+                }
+            };
     }
 }
