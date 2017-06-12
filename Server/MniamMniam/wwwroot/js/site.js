7fbe13af106cfd7a885d53677e65234972377d78
Krzysztof Wrona <krzychuwr1@gmail.com>
2017-06-12 23:20:19 +0200

    Now it is possible to add more ingredients

--- Server/MniamMniam/wwwroot/js/site.js
+++ Server/MniamMniam/wwwroot/js/site.js
@@ -1,1 +1,6 @@
 ﻿// Write your Javascript code.
+$(function () {
+    $('#anotherIngredient').on('click', function () {
+        $('#ingredientsList').append("<div class='form-group'>" + $('#firstIngredient').html() + "</div>")
+    });
+});
