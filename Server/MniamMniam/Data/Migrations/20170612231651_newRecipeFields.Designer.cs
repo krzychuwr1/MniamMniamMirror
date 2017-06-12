7ebfde2f746abd61fcb8e00d6c892de866a61176
Krzysztof Wrona <krzychuwr1@gmail.com>
2017-06-13 01:18:34 +0200

    Added new fields to recipe model and fixed [x] being displayed when there is no image provided for a recipe

--- Server/MniamMniam/Data/Migrations/20170612231651_newRecipeFields.Designer.cs
+++ Server/MniamMniam/Data/Migrations/20170612231651_newRecipeFields.Designer.cs
@@ -8,9 +8,10 @@ using MniamMniam.Data;
 namespace MniamMniam.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170612231651_newRecipeFields")]
+    partial class newRecipeFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
